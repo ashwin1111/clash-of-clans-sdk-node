776c41fc023825aeaa702f239ec57e6691cc8991
ashwin1111 <winash1998@gmail.com>
2020-08-29 21:21:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const ClashOfClansSDK = require('./src/coc');
+
+
+; (async function main() {
+    const connection = await new ClashOfClansSDK(
+        email = process.env.EMAIL || 'yourEmailGoesHere',
+        password = process.env.PASSWORD || 'yourPasswordGoesHere'
+    );
+
+
+    const tokens = await connection.listTokens();
+    console.log(tokens);
+
+    const key = {
+        name: 'name*of*the*key',
+        description: 'description|of|the|key',
+        cidrRanges: ['0.0.0.0'], // Whitelist IP Address 
+        scopes: null
+    };
+    const createdToken = await connection.createTokens(key);
+    console.log(createdToken);
+
+    const deleteKey = await connection.deleteKey('id-goes-here');
+    console.log(deleteKey)
+})()
